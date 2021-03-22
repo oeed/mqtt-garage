602bc6234e63b524395d05430fcc6dded137f221
Oliver Cooper <oliver.cooper@me.com>
2021-03-22 18:45:51 +1300

    Split sensor and remote configs

--- src/door.rs
+++ src/door.rs
@@ -9,22 +9,24 @@ pub mod identifier;
 #[derive(Debug)]
 pub struct Door {
   identifier: Identifier,
+  config: DoorConfig,
   remote_pin: OutputPin,
-  state_pin: Option<InputPin>,
+  sensor_pin: Option<InputPin>,
 }
 
 impl Door {
   pub fn with_config(identifier: Identifier, config: DoorConfig) -> GarageResult<Self> {
     let gpio = Gpio::new()?;
-    let remote_pin = gpio.get(config.remote_pin.pin_number())?.into_output();
-    let state_pin = config
-      .state_pin
-      .map(|state_pin| gpio.get(state_pin.pin_number())?.into_input_pullup());
+    let remote_pin = gpio.get(config.remote.pin.pin_number())?.into_output();
+    let sensor_pin = config
+      .sensor
+      .map(|sensor| gpio.get(sensor.pin.pin_number())?.into_input_pullup());
 
     Door {
       identifier,
+      config,
       remote_pin,
-      state_pin,
+      sensor_pin,
     }
   }
 }
--- src/door/config.rs
+++ src/door/config.rs
@@ -4,14 +4,41 @@ use serde_with::{serde_as, DurationSeconds};
 use std::time::Duration;
 
 #[serde_as]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct DoorConfig {
   /// The name of the MQTT topic to push to
   pub topic_name: String,
-  /// The pin of the door detector sensor (if available)
-  pub state_pin: Option<GpioPin>,
-  /// The pin of the door remote
-  pub remote_pin: GpioPin,
+
   #[serde_as(as = "DurationSeconds<u64>")]
+  /// How long the door is assumed to take to go to/from open/close
   pub travel_time: Duration,
+
+  /// The door detector sensor (if available)
+  pub sensor: Option<SensorConfig>,
+
+  /// The remote used to open and close the door
+  pub remote: RemoteConfig,
+}
+
+
+#[derive(Debug, Deserialize)]
+pub struct SensorConfig {
+  /// The pin of the door detector sensor (if available)
+  pub pin: Option<GpioPin>,
+}
+
+
+#[serde_as]
+#[derive(Debug, Deserialize)]
+pub struct RemoteConfig {
+  /// The pin of the door remote
+  pub pin: GpioPin,
+
+  #[serde_as(as = "DurationSeconds<f64>")]
+  /// How long the remote pin is high for (i.e. how long the remote signal is sent)
+  pub pressed_time: Duration,
+
+  #[serde_as(as = "DurationSeconds<f64>")]
+  /// How long to wait after pressing the remote before pressing another remote
+  pub wait_time: Duration,
 }
